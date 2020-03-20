aa92edc754f1582ee16110dd73235f519e5f0d30
Roshan Sharma <reprogram46@gmail.com>
2020-03-20 14:25:32 +0530

    chore: Render question as component
    This commit introduces a new feature as well - display all question components on single page.

---
+++ src/components/Question.vue
@@ -0,0 +1,60 @@
+<template>
+  <section class="text-center">
+    <q-card class="q-px-md q-mx-lg q-mt-lg">
+      <q-card-section>
+        <div class="text-h6 text-weight-light">
+          {{ q.idx + 1 }} <br />
+          {{ q.body }}
+        </div>
+      </q-card-section>
+    </q-card>
+    <div class="q-pa-md">
+      <q-slider
+        v-model="value"
+        :min="0"
+        :max="10"
+        :step="1"
+        label
+        color="success"
+      />
+    </div>
+    <h2 class="text-h4 text-center q-ml-auto q-mr-auto">
+      <span class="box">{{ value }}</span>
+      / 10
+    </h2>
+    <hr class="q-my-xl" />
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'SingleQuestion',
+  data() {
+    return {
+      value: this.initialValue    // using as data prop; mutation intended
+    }
+  },
+  props: {
+    initialValue: {
+      type: Number,
+      default: 5
+    },
+    q: {                         // to be used directly; no mutation intended
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+.my-card
+  width: 100%
+  max-width: 250px
+
+.box
+  max-width: 200px
+  padding: .2em
+  background-color: $primary
+  color: white
+</style>
--- src/pages/Train.vue
+++ src/pages/Train.vue
@@ -1,49 +1,37 @@
 <template>
-  <q-page class="flex flex-center">
-    <section class="text-center">
-      <q-card class="q-px-md q-ma-lg">
-        <q-card-section>
-          <div class="text-h6 text-weight-light">
-            How much would you rate this person for sense of humour?
-          </div>
-        </q-card-section>
-      </q-card>
-      <div class="q-mt-xl q-pa-lg">
-        <q-slider
-          v-model="value"
-          :min="0"
-          :max="10"
-          :step="1"
-          label
-          color="primary"
-        />
-      </div>
-      <h2 class="text-h4 text-center q-ml-auto q-mr-auto">
-        <span class="box">{{ value }}</span>
-         / 10
-      </h2>
-    </section>
+  <q-page class="flex flex-center">    
+    <question
+      v-for="question in questions"
+      :key="question.idx"
+      :q="question"
+    />
   </q-page>
 </template>
 
 <script>
+import Question from 'components/Question'
 export default {
   data() {
     return {
-      value: 5
+      activeIndex: 0,
+      questions: [
+        {
+          idx: 0,
+          body: 'How much would you rate this person for sense of humour?'
+        },
+        {
+          idx: 1,
+          body: 'How much do you think should this person be rated for anger?'
+        },
+        {
+          idx: 2,
+          body: 'What would you rate this person for mental focus?'
+        }
+      ]
     }
+  },
+  components: {
+    Question
   }
 }
 </script>
-
-<style lang="sass" scoped>
-.my-card
-  width: 100%
-  max-width: 250px
-
-.box
-  max-width: 200px
-  padding: .2em
-  background-color: $primary
-  color: white
-</style>
