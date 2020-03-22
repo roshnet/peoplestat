f96913be89c6217729df89388cb37b307d3c2ff1
Roshan Sharma <reprogram46@gmail.com>
2020-03-22 22:17:32 +0530

    chore[wip]: Add skeletal code to call API
    Perform a POST request and log the response/error.

--- src/pages/Train.vue
+++ src/pages/Train.vue
@@ -21,11 +21,23 @@
       glossy
       round
     />
+    <q-btn
+      @click="sendScores"
+      v-show="activeIndex == questions.length - 1"
+      label="Submit Response"
+      class="q-my-lg"
+      icon="done"
+      size="md"
+      color="primary"
+      glossy
+    />
   </q-page>
 </template>
 
 <script>
+import { endpoints } from 'src/config/api'
 import Question from 'components/Question'
+
 export default {
   components: {
     Question
@@ -55,6 +67,16 @@ export default {
   methods: {
     navigate(action) {
       (action == 'next') ? this.activeIndex += 1 : this.activeIndex -= 1
+    },
+    async sendScores() {
+      await this.$axios
+              .post(endpoints.train, this.$store.state.scores)
+              .then((resp) => {
+                console.log('RESPONSE \n', resp)
+              })
+              .catch((err) => {
+                console.warn('ERROR ', err)
+              })
     }
   }
 }
