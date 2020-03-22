af454a7b214b25a3364d9bfa1dad2d66f652a858
Roshan Sharma <reprogram46@gmail.com>
2020-03-22 22:12:19 +0530

    fix: Define mutations and default score values

--- src/store/index.js
+++ src/store/index.js
@@ -6,11 +6,11 @@ Vue.prototype.$store = vuex
 
 export default new vuex.Store({
   state: {
-    scores: []
+    scores: [5, 5, 5]
   },
   mutations: {
     setScore(state, qObject) {
-        state.scores[qObject.idx] = qObject.score
+      state.scores[qObject.idx] = qObject.score
     }
   }
 })
