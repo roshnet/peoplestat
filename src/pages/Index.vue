cbc55fce10e177bb443a7373e41911237e539d5c
Roshan Sharma <reprogram46@gmail.com>
2020-03-23 13:31:09 +0530

    feat: Add privacy statement; Alter content language

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -3,11 +3,11 @@
     <div class="q-mx-lg">
       <p class="text-subtitle1 text-primary">
         Think of a person you've interacted with.
-        Answer the questions that follow.
+        Set a score on the questions that follow.
       </p>
       <q-btn
         @click="showHint = true"
-        size="15px"
+        size="md"
         color="positive"
         icon="lightbulb"
         round
@@ -15,7 +15,7 @@
       &nbsp;
       <q-btn
         to="/train"
-        size="15px"
+        size="md"
         color="primary"
         icon-right="skip_next"
         label="Start"
@@ -23,7 +23,7 @@
       <q-dialog v-model="showHint">
         <q-card>
           <q-card-section>
-            <div class="text-h6">
+            <div class="text-h6 text-primary">
               <q-icon name="lightbulb" />
               How to use the app
             </div>
@@ -31,6 +31,11 @@
           <q-card-section class="q-pt-none">
             Scores you assign are supposed to be your best guess, regardless of
             any dedicated investigation.
+            <p class="text-subtitle2 text-primary q-mt-md">
+              <strong>Privacy</strong>
+            </p>
+            No labelled data is collected by this app. Whatever you think, stays
+            in your brain. All scores are therefore absolutely anonymous.
           </q-card-section>
           <q-card-actions align="right">
             <q-btn flat label="Got it!" color="primary" v-close-popup />
